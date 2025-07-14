3247518c5c6e4953c23f561e5bb6c19fe9b10b00
dididiri1 <dididiri1@naver.com>
2025-07-14 10:14:39 +0900

    first commit

---
+++ apigetway-service/src/test/java/com/example/apigetwayservice/ApigetwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigetwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigetwayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
